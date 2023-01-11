c492b4018ce0c849329a7b0f83b6a3a7edb4f820
lolokokoo <mano.raichon@etu.univ-amu.fr>
2023-01-11 23:44:25 +0100

    correction customer + viewAcceuil

--- controllers/ControllerProfil.php
+++ controllers/ControllerProfil.php
@@ -11,33 +11,36 @@ class ControllerProfil
         if(!isset($_SESSION['login_id'])) {
             throw new Exception('Customer non trouvé.');
         }
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_POST['modifyCustomer']))){
-            if (isset($_POST["forname"])){
-                $forname = $_POST["forname"];
+        else{
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_POST['modifyCustomer']))){
+                if (isset($_POST["forname"])){
+                    $forname = $_POST["forname"];
+                }
+                if (isset($_POST["surname"])){
+                    $surname = $_POST["surname"];
+                }
+                if (isset($_POST["addone"])){
+                    $addone = $_POST["addone"];
+                }
+                if (isset($_POST["addtwo"])){
+                    $addtwo = $_POST["addtwo"];
+                }
+                if (isset($_POST["addthree"])){
+                    $addthree = $_POST["addthree"];
+                }
+                if (isset($_POST["postcode"])){
+                    $postcode = $_POST["postcode"];
+                }
+                if (isset($_POST["phone"])){
+                    $phone = $_POST["phone"];
+                }
+                if (isset($_POST["email"])){
+                    $email = $_POST["email"];
+                }
+                $this->_CustomerManager->modifyInTable("customers", customers::class,["forname" => $forname , "surname"  => $surname, "add1" => $addone, "add2" => $addtwo, "add3" => $addthree, "postcode" => $postcode, "phone" => $phone, "email" => $email],["id" => $_SESSION['login_id']]);
             }
-            if (isset($_POST["surname"])){
-                $surname = $_POST["surname"];
-            }
-            if (isset($_POST["addone"])){
-                $addone = $_POST["addone"];
-            }
-            if (isset($_POST["addtwo"])){
-                $addtwo = $_POST["addtwo"];
-            }
-            if (isset($_POST["addthree"])){
-                $addthree = $_POST["addthree"];
-            }
-            if (isset($_POST["postcode"])){
-                $postcode = $_POST["postcode"];
-            }
-            if (isset($_POST["phone"])){
-                $phone = $_POST["phone"];
-            }
-            if (isset($_POST["email"])){
-                $email = $_POST["email"];
-            }
-            $this->_CustomerManager->modifyInTable("customers", customers::class,["forname" => $forname , "surname"  => $surname, "add1" => $addone, "add2" => $addtwo, "add3" => $addthree, "postcode" => $postcode, "phone" => $phone, "email" => $email],["id" => $_SESSION['login_id']]);
         }
+
 
         $customer = $this->_CustomerManager->getOneCustomer($_SESSION['login_id']);
 
--- views/viewAcceuil.php
+++ views/viewAcceuil.php
@@ -2,4 +2,26 @@
     $this->_t = 'Mon Blog'; 
 ?>
 
-<h1> bienvenue </h1>
+<h1><strong>Bienvenue sur Web4Shop</strong></h1>
+<h3 class="mb-3">Vous trouverez dans les diférentes rubriques l'ensemble de nos produits</h3>
+<div class="row">
+    <div class="col-12 col-sm-4 col-md-4 col-xl-4">
+        <a href="index.php?url=categories&cat=1">
+            <img class="image-accueil" src="productimages/jusCitron.jpg" alt="Photo indisponible">
+            <p class="text-center"><strong>Nos boissons</strong></p>
+        </a>
+    </div>
+    <div class=" col-12 col-sm-4 col-md-4 col-xl-4">
+        <a href="index.php?url=categories&cat=2">
+            <img class="image-accueil" src="productimages/biscuitsTortue.jpg" alt="Photo indisponible">
+            <p class="text-center"><strong>Nos biscuits</strong></p>
+        </a>
+    </div>
+    <div class=" col-12 col-sm-4 col-md-4 col-xl-4">
+        <a href="index.php?url=categories&cat=3">
+            <img class="image-accueil" src="productimages/abricotsSecs.jpg" alt="Photo indisponible">
+            <p class="text-center"><strong>Nos Fruits Secs</strong></p>
+        </a>
+    </div>
+</div>
+
